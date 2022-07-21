2a3374be54627d3d34503cbeaf42a4bf6652f72f
Rafael Pereira <rafaelhpmg@gmail.com>
2022-07-21 01:17:48 -0300

    :bug: Fix pomodoro timer props

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,10 +11,10 @@ const Home: NextPage = () => {
       <main className="max-w-md my-auto mx-auto flex flex-col justify-center items-center h-screen">
         <div className="container bg-gray-200 text-black text-center my-5 mx-2 max-w-sm p-10 rounded-lg shadow-lg">
           <PomodoroTimer
-            pomodoroTime={1}
-            shortRestTime={1}
-            longRestTime={5}
-            pomodoroCycles={4}
+            pomodoroTime={1500} // 25 minutos
+            shortRestTime={300} // 5 minutos
+            longRestTime={900} // 15 minutos
+            pomodoroCycles={4} // 4 ciclos
           />
         </div>
       </main>
